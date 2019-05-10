2f279e8bb47004f55ee6833abeafdd7d0fed9ef1
abdulwahabfarooq <50476125+abdulwahabfarooq@users.noreply.github.com>
2019-05-10 13:11:20 +0500

    Update fcfs.cpp

--- fcfs.cpp
+++ fcfs.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
-    cout<<"Enter total number of processes(maximum 20):";
+    cout<<"Determine total number of processes:";
     cin>>n;
  
     cout<<"\nEnter Process Burst Time\n";
@@ -15,9 +15,9 @@ int main()
         cin>>bt[i];
     }
  
-    wt[0]=0;    //waiting time for first process is 0
+    wt[0]=0;    
  
-    //calculating waiting time
+   
     for(i=1;i<n;i++)
     {
         wt[i]=0;
@@ -27,7 +27,6 @@ int main()
  
     cout<<"\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time";
  
-    //calculating turnaround time
     for(i=0;i<n;i++)
     {
         tat[i]=bt[i]+wt[i];
